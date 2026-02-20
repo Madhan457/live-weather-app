703cb3f7be773fd1f9a10cfedd6d3162bf9ebae3
Madhan Sankar <madhansankar2008s@gmail.com>
2026-02-20 16:07:08 +0530

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,55 @@
+const apikey = "2590e4d58f1ff09e6ae80d029d7a307c";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchbox = document.getElementById("city-input");
+const searchbtn = document.getElementById("city-btn");
+const weatherIcon = document.getElementById("weather-icon");
+async function weather(city) {
+  try {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+
+    var data = await response.json();
+    document.getElementById("display-city").innerHTML = data.name;
+    document.getElementById("degree").innerHTML = data.main.temp;
+    document.getElementById("wind-value").innerHTML = data.wind.speed;
+    document.getElementById("humidity-percentage").innerHTML =
+      data.main.humidity;
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "/linkedin/weather/images/weather1.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "/linkedin/weather/images/sun.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "/linkedin/weather/images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "/linkedin/weather/images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "/linkedin/weather/images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "/linkedin/weather/images/snow.png";
+    } else {
+      weatherIcon.src = "/linkedin/weather/images/weather1.png";
+    }
+  } catch (error) {
+    alert("City Not found");
+  }
+}
+searchbtn.addEventListener("click", () => {
+  const city = searchbox.value.trim();
+  if (city === "") {
+    alert("Please enter city:");
+    return;
+  }
+  weather(city);
+  searchbox.value = "";
+});
+searchbox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    searchbtn.click();
+  }
+});
+window.addEventListener("load", () => {
+  weather("chennai");
+});
